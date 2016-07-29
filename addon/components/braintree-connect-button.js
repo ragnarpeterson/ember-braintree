8891d10952fb14232b052a651c3de5378db1d0f0
James Dixon <jim.w.dixon@gmail.com>
2016-07-28 21:35:40 -0600

    tidy up connect button

--- addon/components/braintree-connect-button.js
+++ addon/components/braintree-connect-button.js
@@ -8,6 +8,7 @@ export default Ember.Component.extend(InvokeActionMixin, {
   classNames: ['braintree-connect-btn'],
 
   connectUrl: null,
+  options: {},
 
 
   didInsertElement() {
@@ -16,24 +17,29 @@ export default Ember.Component.extend(InvokeActionMixin, {
 
     const connectUrl = this.get('connectUrl');
 
+    // Ensure that a connect url was passed
     if (connectUrl) {
 
+      // Setup options
       const options = Object.assign({
         connectUrl: connectUrl,
         container: this.get('elementId')
       }, this.get('options'));
 
+      // Add the onError action if it was passed
       if (this.get('onError') !== undefined) {
         options.onError = (...args) => {
           Ember.run.schedule('actions', this, () => {
-            this.invokeAction(eventName, ...args, this.$());
+            this.invokeAction('onError', ...args);
           });
         };
       }
 
+      // Create the Braintree Connect button
       return new BraintreeOAuthConnect(options);
     }
 
+    // Throw an error if no connect url
     throw new Error('a valid connect url is required');
   },
 
