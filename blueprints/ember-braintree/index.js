7672bd61bfbefe6155ef7ea8709175b8947d868f
James Dixon <jim.w.dixon@gmail.com>
2016-07-29 19:32:23 -0600

    fix bower dependency syntax

--- blueprints/ember-braintree/index.js
+++ blueprints/ember-braintree/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  
+
   normalizeEntityName: function() {
     // this prevents an error when the entityName is
     // not specified (since that doesn't actually matter
@@ -11,8 +11,8 @@ module.exports = {
   afterInstall: function() {
 
     const packages = [
-      { name: 'braintree-web', '~2.27.0' },
-      { name: 'braintree-oauth-connect', target: '~1.0.0' }
+      { name: 'braintree-web', target: '~2.27.0' },
+      { name: 'braintree-oauth-connect', target: '*' }
     ]
 
     return this.addBowerPackagesToProject(packages);
